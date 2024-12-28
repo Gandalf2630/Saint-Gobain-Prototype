bf498c20037f6447136b600ef58842ec57abed6f
Durmus Arkin <durm0008@edu.zealand.dk>
2024-12-29 00:11:49 +0100

    Færdiggjort navbar + igang med hovedside

---
+++ src/components/navbar.js
@@ -0,0 +1,25 @@
+import Link from "next/link"
+export default function Navbar() {
+    return (
+        <div className="flex h-44 justify-between min-w-screen">
+            <nav className="flex justify-between items-center px-28 py-6 w-full">
+                <div className="flex items-center">
+                    <Link href="/"><img src="/assets/logo.png"/> </Link>
+                </div>
+                <ul className="flex space-x-8 text-2xl">
+
+                    <li className="hover:text-yellow-400">
+                    <Link href="/produkter">Produkter</Link>
+                    </li>
+
+                    <li className="hover:text-yellow-400">
+                       <Link href="/hjælp">Hjælp</Link>
+                    </li>
+                    <li className="hover:text-yellow-400">
+                       <Link href="/info">Info</Link> 
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
